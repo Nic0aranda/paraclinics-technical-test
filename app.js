b2392ad3444609b181e4f895cb9ca43dd96e8c78
nico <ale.aranda@duocuc.cl>
2026-09-22 12:57:26 -0300

    variables js

---
+++ app.js
@@ -0,0 +1,9 @@
+//variables para consumo de productos
+let todosLosProductos = [];
+const URL = 'https://dummyjson.com/products';
+
+//variables DOM
+const cuerpoTabla = document.getElementById('cuerpo-tabla');
+const contenedorTabla = document.getElementById('contenedor-tabla');
+const buscador = document.getElementById('buscador');
+const contadorTexto = document.getElementById('contador');
